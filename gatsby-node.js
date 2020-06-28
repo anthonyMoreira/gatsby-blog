b8ec069ac6f8ece477d1b636e6037f79daf9e3f7
anthonyMoreira <anthony@moreira.tech>
2020-06-28 22:39:43 +0200

    Adding remark42 comment component

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,62 +3,57 @@ const path = require('path')
 const _ = require('lodash')
 
 exports.createPages = ({ graphql, actions }) => {
-	const { createPage } = actions
+  const { createPage } = actions
 
-	return new Promise((resolve, reject) => {
-		const blogPost = path.resolve('./src/templates/blog-post.js')
-		const tagTemplate = path.resolve('./src/templates/tags.js')
-		resolve(
-			graphql(
-				`
-					{
-						allContentfulBlogPost {
-							edges {
-								node {
-									title
-									slug
-								}
-							}
-						}
-						tagsGroup: allContentfulBlogPost(
-							limit: 2000
-						) {
-							group(field: tags) {
-								fieldValue
-							}
-						}
-					}
-				`
-			).then(result => {
-				if (result.errors) {
-					console.log(result.errors)
-					reject(result.errors)
-				}
+  return new Promise((resolve, reject) => {
+    const blogPost = path.resolve('./src/templates/blog-post.js')
+    const tagTemplate = path.resolve('./src/templates/tags.js')
+    resolve(
+      graphql(
+        `
+          {
+            allContentfulBlogPost {
+              edges {
+                node {
+                  title
+                  slug
+                }
+              }
+            }
+            tagsGroup: allContentfulBlogPost(limit: 2000) {
+              group(field: tags) {
+                fieldValue
+              }
+            }
+          }
+        `
+      ).then((result) => {
+        if (result.errors) {
+          console.log(result.errors)
+          reject(result.errors)
+        }
 
-				const posts =
-					result.data.allContentfulBlogPost.edges
-				posts.forEach((post, index) => {
-					createPage({
-						path: `/blog/${post.node.slug}/`,
-						component: blogPost,
-						context: {
-							slug: post.node.slug,
-						},
-					})
-				})
-				const tags = result.data.tagsGroup.group
-				tags.forEach((tag, index) => {
-					createPage({
-						path: `/tags/${_.kebabCase(
-							tag.fieldValue
-						)}`,
-						component: tagTemplate,
-						context: {
-							tag: tag.fieldValue,
-						},
-					})
-				})
-			})
-		)
-	})
+        const posts = result.data.allContentfulBlogPost.edges
+        posts.forEach((post, index) => {
+          createPage({
+            path: `/blog/${post.node.slug}/`,
+            component: blogPost,
+            context: {
+              slug: post.node.slug,
+            },
+          })
+        })
+        const tags = result.data.tagsGroup.group
+        tags.forEach((tag, index) => {
+          createPage({
+            path: `/tags/${_.kebabCase(tag.fieldValue)}`,
+            component: tagTemplate,
+            context: {
+              tag: tag.fieldValue,
+            },
+          })
+        })
+      })
+    )
+  })
 }
